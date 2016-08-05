71639ffdab9bdc75296d2630e8a6a75706b0bce2
bfricks <bfricks@cequint.com>
2016-08-05 10:23:00 -0700

    updated file structure and fixed android project loading errors

---
+++ WafflePirate/app/src/main/java/com/netfricks/wafflepirate/DrivingActivity.java
@@ -0,0 +1,13 @@
+package com.netfricks.wafflepirate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrivingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driving);
+    }
+}
