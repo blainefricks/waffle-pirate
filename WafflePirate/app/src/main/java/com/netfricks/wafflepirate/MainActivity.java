a2bc3548cbcfe03dea985f9e4ea4d3b577cdb760
bfricks <bfricks@cequint.com>
2016-08-05 21:45:22 -0700

    I have no idea what i'm doing...

--- WafflePirate/app/src/main/java/com/netfricks/wafflepirate/MainActivity.java
+++ WafflePirate/app/src/main/java/com/netfricks/wafflepirate/MainActivity.java
@@ -4,13 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mainListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainListView = (ListView) findViewById(R.id.mainListView);
+
+        String[] mainOptions = new String[] {"Driving Settings","Calendar Settings"};
+
+        ArrayList<String> mainOptionsList = new ArrayList<String>();
+        mainOptionsList.addAll(Arrays.asList(mainOptions));
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.list_item, mainOptionsList);
+
+        mainListView.setAdapter(listAdapter);
     }
 
     public void viewActivityDriving(View view) {
