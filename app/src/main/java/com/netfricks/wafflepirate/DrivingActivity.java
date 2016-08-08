24ba4eeef71c624a3b7f57d1273c658af4432967
bfricks <bfricks@cequint.com>
2016-08-08 11:01:52 -0700

    started work on checking for missing permissions.

--- app/src/main/java/com/netfricks/wafflepirate/DrivingActivity.java
+++ app/src/main/java/com/netfricks/wafflepirate/DrivingActivity.java
@@ -1,5 +1,9 @@
 package com.netfricks.wafflepirate;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,21 @@ public class DrivingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving);
+        permissionCheck();
+    }
+
+    public void permissionCheck() {
+        int permissionCheckBluetooth = ContextCompat.checkSelfPermission(DrivingActivity, Manifest.permission.BLUETOOTH);
+
+        if (permissionCheckBluetooth != PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            ActivityCompat.requestPermissions(DrivingActivity, new String[]{Manifest.permission.BLUETOOTH}, MY_PERMISSIONS_REQUEST_BLUETOOTH);
+
+            // MY_PERMISSIONS_REQUEST_BLUETOOTH is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            // TODO: make MY_PERMISSIONS_REQUEST_BLUETOOTH an app-defined int constant?
+        }
     }
 }
